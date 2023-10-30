6eff835852b25b73baa86719e80d5563b22ff0c9
hoejsagerc <christian@hoejsager.com>
2023-10-30 09:32:09 +0100

    updates

---
+++ csharp/1356_sort_integers_by_the_number_of_1_bits/L1356/L1356/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Collections;
+
+
+var myArray = new int[] {1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1};
+
+SortByBits(myArray);
+
+int[] SortByBits(int[] arr)
+{
+    var map = new List<Dictionary<int, int>>();
+
+    foreach (var i in arr)
+    {
+        Console.WriteLine("");
+        Console.WriteLine($"Checking bits in number: {i}");
+        BitArray barray = new BitArray(new int[] { i });
+        var count = 0;
+        foreach (var bit in barray)
+        {
+            if (bit.Equals(true))
+            {
+                Console.Write("1 ");
+                count++;
+            }
+            else
+            {
+                Console.Write("0 ");
+            }
+        }
+
+        map.Add(new Dictionary<int, int>(){{ i, count }});
+    }
+
+    // var sortedList = map.Select(dict => dict.OrderBy(kvp => kvp.Value).ToDictionary(kvp => kvp.Key, kvp => kvp.Value));
+    var sortedList = map.OrderBy(x => x.Values.Max()).ThenBy(x => x.Keys.FirstOrDefault()).ToList();
+    var resultArray = new List<int>();
+    foreach (var x in sortedList)
+    {
+        resultArray.Add(x.Keys.FirstOrDefault());
+    }
+
+    return resultArray.ToArray();
+}
