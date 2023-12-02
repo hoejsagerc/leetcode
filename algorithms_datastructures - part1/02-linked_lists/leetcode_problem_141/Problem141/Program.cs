253da402310eab0602f087cf196c9ea1bb611599
hoejsagerc <christian@hoejsager.com>
2023-12-02 21:22:10 +0100

    added aoc 2023 d2

---
+++ algorithms_datastructures - part1/02-linked_lists/leetcode_problem_141/Problem141/Program.cs
@@ -0,0 +1,31 @@
+﻿
+  // Definition for singly-linked list.
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+
+// Solution
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        var slow = head;
+        var fast = head;
+
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+            {
+                return true; // fast caught up with slow meaning there is a loop
+            }
+        }
+
+        return false; // fast reached a null meaning the end of the linked list
+    }
+}
