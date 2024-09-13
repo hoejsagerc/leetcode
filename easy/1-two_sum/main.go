4135d1b27ee1a5f6ffdecd72b575bbee50e77226
“Christian” <chho@danofficeit.com>
2024-09-13 21:16:30 +0200

    documented leetcode-1

--- easy/1-two_sum/main.go
+++ easy/1-two_sum/main.go
@@ -1,15 +1,26 @@
 package main
 
 func twoSum(nums []int, target int) []int {
-	numToIndexMap := make(map[int]int)
+	// create a new hashmap
+	indexMap := make(map[int]int)
 
+	// loop through the nums array
 	for i, num := range nums {
-		var diff int = target - num
-		if index, ok := numToIndexMap[diff]; ok {
-			return []int{index, i}
+		// get the diff between the target and the num in array
+		diff := target - num
+
+		// check if the diff exists in the hashmap if the difference between the current
+		// element and the target already exists in the hashmap, then we know index of that value
+		if idx, ok := indexMap[diff]; ok {
+			// return array with the index of the found element in the hashmap
+			// and the current element in the arrays index
+			return []int{idx, i}
 		} else {
-			numToIndexMap[num] = i
+			// if difference was not found in hashmap, then add the current element from the array
+			// to the hashmap with the element as the key, and the elementes index as the value
+			indexMap[num] = i
 		}
 	}
+	// return nil if nothing found
 	return nil
 }
